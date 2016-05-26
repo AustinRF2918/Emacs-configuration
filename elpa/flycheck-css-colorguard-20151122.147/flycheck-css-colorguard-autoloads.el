25033d1fb514881fb513007a728f641f619f4a1c
Austin Fell <austinrf2918@yahoo.com>
2016-05-26 09:23:52 -0400

    First commit

---
+++ elpa/flycheck-css-colorguard-20151122.147/flycheck-css-colorguard-autoloads.el
@@ -0,0 +1,15 @@
+;;; flycheck-css-colorguard-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-css-colorguard.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-css-colorguard-autoloads.el ends here
---
+++ elpa/flycheck-css-colorguard-20151122.147/flycheck-css-colorguard-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-css-colorguard" "20151122.147" "Detect similar colors in CSS" '((flycheck "0.22") (emacs "24")) :url "https://github.com/Simplify/flycheck-css-colorguard/" :keywords '("flycheck" "css" "colorguard"))
